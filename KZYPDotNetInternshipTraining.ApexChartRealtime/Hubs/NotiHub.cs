258d741020520f013854ee62f78f2ad3595a8d2e
KyawZayarPhyo-22 <22kyawzayarphyo@gmail.com>
2026-06-10 09:44:42 +0630

    Apex update

---
+++ KZYPDotNetInternshipTraining.ApexChartRealtime/Hubs/NotiHub.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace KZYPDotNetInternshipTraining.ApexChartRealtime.Hubs;
+
+public class NotiHub : Hub
+{
+}
---
+++ KZYPDotNetInternshipTraining.ApexChartRealtime/Models/TeamModel.cs
@@ -0,0 +1,8 @@
+﻿namespace KZYPDotNetInternshipTraining.ApexChartRealtime.Models;
+
+public class TeamModel
+{
+    public int TeamId { get; set; }
+    public string TeamName { get; set; }
+    public int TeamValue { get; set; }
+}
